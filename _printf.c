466a8da9694ef07bc1fcadf5ebfeac3ef59603b4
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-09-19 22:27:35 -0700

    A function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - function that produces output according to a format.
  * @format: character string
@@ -8,23 +7,17 @@
 int _printf(const char *format, ...)
 {
 	int i, pr_char, cal_len;
-	char c_char;
-	char *st_r;
+	char *st_r, c_char;
 	va_list ls_args;
+
 	va_start(ls_args, format);
-
 	if (format == NULL)
-		return(-1);
-
-	pr_char = 0;
-	i = 0;
+		return (-1);
+	i = pr_char = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] != '%')	
-		{
-			write(1, &format[i], 1);
-			pr_char++;
-		}
+		if (format[i] != '%')
+			write(1, &format[i], 1), pr_char++;
 		else
 		{
 			i++;
@@ -33,26 +26,24 @@ int _printf(const char *format, ...)
 			if (format[i] == 'c')
 			{
 				c_char = va_arg(ls_args, int);
-				write(1, &c_char, 1);
-				pr_char++;
+				write(1, &c_char, 1), pr_char++;
 			}
-			else if (format[i] == 's') 
+			else if (format[i] == 's')
 			{
 				st_r = va_arg(ls_args, char*);
 				cal_len = 0;
 				while (st_r[cal_len] != '\0')
-					cal_len += cal_len;
+				{
+					cal_len ++;
+				}
 				write(1, st_r, cal_len);
 				pr_char += cal_len;
 			}
 			else if (format[i] == '%')
-			{
-				write(1, &format[i], 1);
-				pr_char++;
-			}
+				write(1, &format[i], 1), pr_char++;
 		}
 		i++;
 	}
 	va_end(ls_args);
-	return pr_char;
+	return (pr_char);
 }
--- main.c
+++ main.c
@@ -9,17 +9,14 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    return (0);
+	_printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
+	return (0);
 }
